2943fdb5feffba01e7897b57c2f48b0e6cae2d76
Unknown77 <riccowiguna1@gmail.com>
2021-11-23 13:03:05 +0700

    no message

---
+++ Assets/Script/SawObstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawObstacle : MonoBehaviour
+{
+    CharacterMove move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        move = col.gameObject.GetComponent<CharacterMove>();
+        col.gameObject.SetActive(false);
+        move.GameOver();
+                
+    }
+}
